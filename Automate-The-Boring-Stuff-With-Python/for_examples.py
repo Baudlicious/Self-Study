ea3c172813b757d5e62a00180fd564128f15e3e9
x3830s <x3830s@protonmail.com>
2018-06-08 20:23:58 -0400

    'for_examples.py'

--- Automate-The-Boring-Stuff-With-Python/for_examples.py
+++ Automate-The-Boring-Stuff-With-Python/for_examples.py
@@ -4,3 +4,14 @@
 print('My name is')
 for i in range(5):
     print('Jimmy Five Times {}'.format(i))
+
+total = 0
+for num in range(101):
+    total = total + num
+
+print(total)
+
+# This one loops backwards and determines the 'step'
+print('My name is')
+for i in range(5, -1, -1):
+    print('Jimmy Five Times {}'.format(i))
