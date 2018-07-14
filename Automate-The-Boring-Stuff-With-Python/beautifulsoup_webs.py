e8a3d5249c774d80f0bf87a2b8b68e6949eb07e8
x3830s <x3830s@protonmail.com>
2018-07-14 07:46:44 -0400

    webscrape

---
+++ Automate-The-Boring-Stuff-With-Python/beautifulsoup_webs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# This is x3830s python files from the book Automate Python The
+# Boring Stuff with Python. 
+
+def title(big_title, date):
+    print('!!!!' + big_title.upper().center(70) + ' !!!!')
+    print(date.upper().center(80))
+
+def sep(title):
+    print("-" * 80)
+    print('[ + ] ' + title.title())
+    print("-" * 80 +"\n")
+
+def function(func):
+    print(">> " + func + ":")
+
+
