9f81e035763343fbee933f7f42e54f7d8359f893
x3830s <x3830@protonmail.com>
2018-07-16 09:12:21 -0400

    bs4

--- Automate-The-Boring-Stuff-With-Python/beautifulsoup_webs.py
+++ Automate-The-Boring-Stuff-With-Python/beautifulsoup_webs.py
@@ -14,4 +14,19 @@ def sep(title):
 def function(func):
     print(">> " + func + ":")
 
+title('Beautiful Soup', '16/07/18')
+sep('URL is broken here but I have experience with bs4 so im not concerned with fixing')
 
+import bs4
+import requests
+
+res = requests.get('http://www.amazon.com/Automate-Boring-Stuff-Python-Programming/dp/1593275994/')
+
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+soup.select('#mediaNoAccordion > div.a-row > div.a-column.a-span7.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price')
+
+elems = soup.select('#mediaNoAccordion > div.a-row > div.a-column.a-span7.a-text-right.a-span-last > span.a-size-medium.a-color-price.header-price')
+print(elems)
+#print(elems[0].text.strip())
