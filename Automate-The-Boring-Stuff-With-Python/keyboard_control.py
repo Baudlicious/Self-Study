51ba41278edb487480102cc85ce552f8283ea616
x3830s <x3830@protonmail.com>
2018-07-19 08:05:48 -0400

    Keyboard control

---
+++ Automate-The-Boring-Stuff-With-Python/keyboard_control.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# This is x3830s python files from the book Automate Python The
+# Boring Stuff with Python. 
+
+def title(big_title, date):
+    print('!!!!' + big_title.upper().center(70) + ' !!!!')
+    print(date.upper().center(80))
+
+def sep(title):
+    print("-" * 80)
+    print('[ + ] ' + title.title())
+    print("-" * 80 +"\n")
+
+def function(func):
+    print(">> " + func + ":")
+
+title('Controlling the keyboard with pyautogui', '19/07/18')
+
+sep('The module pyautogui.typewrite("text")')
+print('pyautogui.typewrite("Hello World!")')
