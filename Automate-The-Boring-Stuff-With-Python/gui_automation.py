abf8845ff07b7119878147cec9b7577df8cb56f5
x3830s <x3830@protonmail.com>
2018-07-19 07:58:42 -0400

    GUI automation

--- Automate-The-Boring-Stuff-With-Python/gui_automation.py
+++ Automate-The-Boring-Stuff-With-Python/gui_automation.py
@@ -14,4 +14,24 @@ def sep(title):
 def function(func):
     print(">> " + func + ":")
 
+title('GUI automation using pyautogui', '19/07/18')
 
+import pyautogui
+sep('To return the screen resolution we can use pyautogui.size()')
+print('width, height = pyautogui.size()')
+
+sep('To look at the current mouse position we can use pyautogui.position()')
+print('x, y = pyautogui.position()')
+
+sep('TO move the mouse we use pyautogui.moveTo(x, y)')
+print('pyautogui.moveTo(10,10)')
+print('pyautogui.moveTo(10,10, duration=1.5)')
+
+sep('To have the mouse click use pyautogui.click(x,y)')
+print('pyautogui.click(339, 38)')
+print('pyautogui.Doubleclick(339, 38)')
+print('pyautogui.Doubleclick()')
+
+sep('THere is a failsafe that allows you to put the mouse in the top left to stop')
+
+sep('With the pyautogui.displayMousePosition() we can see realtime coordinates for mouse position')
